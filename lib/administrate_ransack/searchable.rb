0276b0243359378a30f1e65496f2d041ddeb41c3
nakamura <shop@goosys.sakura.ne.jp>
2024-07-26 07:02:25 +0000

    feat: Add invalid search term handling

--- lib/administrate_ransack/searchable.rb
+++ lib/administrate_ransack/searchable.rb
@@ -6,7 +6,13 @@ module AdministrateRansack
   module Searchable
     def scoped_resource
       options = respond_to?(:ransack_options) ? ransack_options : {}
-      @ransack_results = super.ransack(params[:q], **options)
+      begin
+        @ransack_results = super.ransack(params[:q], **options)
+      rescue ArgumentError => e
+        handle_ransack_argument_error(e)
+        set_flash_message_as_ransack_argument_error(e)
+        @ransack_results = reset_ransack_result_on_error(super).ransack({}, **options)
+      end
       @ransack_results.result(distinct: true)
     end
 
@@ -23,6 +29,33 @@ module AdministrateRansack
       def prepended(base)
         base.helper_method :sanitized_order_params
       end
+    end
+
+    private
+
+    def set_flash_message_as_ransack_argument_error(error)
+      if error.message.eql?("Invalid sorting parameter provided")
+        flash.now[:alert] = I18n.t(
+          :invalid_sorting_parameter_provided,
+          scope: [:administrate_ransack, :errors],
+          default: error.message
+        )
+      elsif error.message.start_with?("Invalid search term ")
+        flash.now[:alert] = I18n.t(
+          :invalid_search_term,
+          search_term: error.message.split(' ')[3..].join(' '),
+          scope: [:administrate_ransack, :errors],
+          default: error.message
+        )
+      end
+    end
+
+    def handle_ransack_argument_error(error)
+      super if defined?(super)
+    end
+
+    def reset_ransack_result_on_error(super_scoped_resource)
+      super_scoped_resource.none
     end
   end
 end
